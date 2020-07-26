bd95591bbe7688ccedacb43d9bd2db50ef1b3a08
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-07-26 18:56:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-  results = [1,2,3]
-  array.each { |i| results << i ** 2 }
-  results
-
+  array.each_with_object([]) { |i,arr| arr << i ** 2 }
 end
+my_arr = [1, 2]
+p square_array(my_arr)
+
